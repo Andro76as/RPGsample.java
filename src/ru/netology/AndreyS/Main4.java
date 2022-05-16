5cc669a4b83a568baac99922dacb672321660219
Andro76as <andro76as@gmail.com>
2022-05-16 21:33:22 +0300

    Commit1

--- src/ru/netology/AndreyS/Main4.java
+++ src/ru/netology/AndreyS/Main4.java
@@ -22,29 +22,40 @@ public class Main4 {
             Scanner scanner = new Scanner(in);
             while (scanner.hasNext()) {
                 list.add(scanner.next("\\s*,\\s*\\s*."));
-            }
-
-            Map<String, Integer> map = new HashMap<>(); // для подсчёта вхождений
-            Map.Entry<String, Integer> maxEntry = null; // будет хранить слово с максимальным числом вхождений
 
 
-            // "Подсчёт слов" с помощью hashmap
-            for (String val : list) { // обходим некоторую коллекцию
-                if (map.containsKey(val)) { // если "слово" уже есть
-                    map.put(val, (map.get(val) + 1)); // наращиваем счётчик для данного слова
-                } else {// если такое слово не известно
-                    map.put(val, (1)); // добавляем в индекс со значением = 1
+                Map<String, Integer> map = new HashMap<>(); // для подсчёта вхождений
+                Map.Entry<String, Integer> maxEntry = null; // будет хранить слово с максимальным числом вхождений
+
+
+                // "Подсчёт слов" с помощью hashmap
+                for (String val : list) { // обходим некоторую коллекцию
+                    if (map.containsKey(val)) { // если "слово" уже есть
+                        map.put(val, (map.get(val) + 1)); // наращиваем счётчик для данного слова
+                    } else {// если такое слово не известно
+                        map.put(val, (1)); // добавляем в индекс со значением = 1
+
+                    }
+
+                    // не видит???
+                    map.toString(); // не видит???
+                    System.out.println(map); // не видит???
+
+                    // не видит???
+                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                        System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+                    }
                 }
+
+
+                // обойдём коллекцию и найдём максимальное значение
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                    // если максимум не определен или очередное значение ещё больше
+                    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                        maxEntry = entry; // то присваиваем максимуму новое значение
+                    }
+                } // после завершения цикла в maxEntry лежит элемент с максимальным value (значением)
             }
-
-            // обойдём коллекцию и найдём максимальное значение
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                // если максимум не определен или очередное значение ещё больше
-                if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-                    maxEntry = entry; // то присваиваем максимуму новое значение
-                }
-            } // после завершения цикла в maxEntry лежит элемент с максимальным value (значением)
-
         }
     }
 
